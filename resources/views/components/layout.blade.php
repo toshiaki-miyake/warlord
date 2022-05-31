3ae381c39b421ab685e1b59519a7bf2721b11adf
Toshiaki Miyake <t.myk444@gmail.com>
2022-05-31 20:24:19 +0900

    ログイン完了

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -14,7 +14,7 @@
                 @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                 @auth
-                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">管理画面</a>
+                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ダッシュボード</a>
                 @else
                     <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ログイン</a>
 
--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,15 @@ use App\Http\Controllers\CommentController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
-
 
 Route::get('/', [PostController::class, 'index'])
     ->name('posts.index');
